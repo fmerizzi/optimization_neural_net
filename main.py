4e0368ac5bed877dbc57d5e651b03cd585315671
fmerizzi <merizzifabio@gmail.com>
2021-03-24 17:40:30 +0100

    remove main

--- main.py
+++
@@ -1,9 +0,0 @@
-import net
-import mnist_loader
-
-net = net.Net([784, 10, 10])
-
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-
-
-net.SGD(training_data, 3, 250, 6.0, test_data=test_data)
