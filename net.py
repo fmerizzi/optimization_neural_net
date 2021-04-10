021707e163c60ec8a4993931b60edd2bc60c4c4e
fmerizzi <merizzifabio@gmail.com>
2021-04-10 10:21:32 +0200

    weigths

--- net.py
+++ net.py
@@ -5,22 +5,35 @@ import loader
 import time 
 import copy
 
-
 def set_biases(bias_matrix_dim,rnd):
-	return [rnd.randn(y, 1) for y in bias_matrix_dim]
+    tmp = []
+    for i in bias_matrix_dim:
+        tmp.append(rnd.randn(i, 1))
+    return tmp
     
 def set_weights(weight_matrix_dim,rnd):
-	return [rnd.randn(y, x) for x, y in zip(weight_matrix_dim[0],weight_matrix_dim[1])]
+    tmp = []
+    for i in weight_matrix_dim:
+        tmp.append(rnd.randn(i[1], i[0]))
+    return tmp
 
 def set_weights_unsaturated(weight_matrix_dim,rnd):
-    # by dividing the weight by the squared number of neurons we avoid over saturation
-    # squish the gaussian 
-	return [rnd.randn(y, x)/np.sqrt(x)  for x, y in zip(weight_matrix_dim[0],weight_matrix_dim[1])]
-
+    tmp = []
+    for i in weight_matrix_dim:
+        tmp.append(rnd.randn(i[1], i[0])/np.sqrt(i[0]))
+    return tmp
+    
 def split_data(training_data,mini_batch_size): 
-            n = len(training_data)
-            return [training_data[k:k+mini_batch_size]for k in range(0, n, mini_batch_size)]
-        
+    tmp = []
+    for i in range(0,len(training_data),mini_batch_size):
+        tmp.append(training_data[i:i+mini_batch_size])
+    return tmp
+
+def prepare_weightMatrix_dim(sizes):
+    tmp = []
+    for i in range(len(sizes)-1):
+        tmp.append([sizes[i],sizes[i+1]])
+    return tmp
         
 def plot_biases(biases_log):
     
@@ -98,7 +111,7 @@ def train(training_data, epochs, mini_batch_size, eta,test_data,weights,biases,r
 	        # Update paramenters for each mini batch
             for mini_batch in mini_batches:
             		
-            		# Prepare the arrays for the partial derivatives of weights and biases
+            		# Prepare the lists for the partial derivatives of weights and biases
                     partial_deriv_biases = [np.zeros_like(b) for b in biases]
                     partial_deriv_weights = [np.zeros_like(w) for w in weights]
 
@@ -179,7 +192,7 @@ def backprop(net_input, y,biases,weights,cross_entropy):
             
         deriv_bias[-1] = delta
         #BACKWARD RUN 
-	# and then all the others running backward 
+	    # and then all the others running backward 
         for l in range(-2, -len(sizes),-1):
             z = zs[l]
             sigma_prime = sigmoid_deriv(z)
@@ -199,6 +212,8 @@ def crossCost_derivarive(output_activations, y,z):
         return (output_activations-y) 
 
 def evaluate(test_data,biases,weights):
+        # TODO 
+        # In a better way
         test_results = [(np.argmax(activation(x,biases,weights)), y)
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
@@ -222,22 +237,36 @@ def sigmoid_deriv(z):
 
 ###############################################################################################################
 
+# PARAMETER SETTING 
 #Set network architecture
-sizes = [784,28,28,10]
-num_layers = len(sizes)
+sizes = [784,10,10]
+#Set if unsaturated weights
+unsaturated_weights = True
+# Hyperparameter set 
+epochs = [5,5]
+mini_batches = [100,200]
+eta = [1.5,3]
 
-#prepare the bias/weights structures 
+###############################################################################################################
+num_layers = len(sizes)
+# Prepare biases for each level except the first 
 bias_matrix_dim = sizes[1:]
-weight_matrix_dim = [sizes[:-1], sizes[1:]]
+#weight_matrix_dim = [sizes[:-1], sizes[1:]]
+weight_matrix_dim = prepare_weightMatrix_dim(sizes)
+
 
 print("bias matrix dim", bias_matrix_dim)
 print("weight matrix dim",weight_matrix_dim)
 
-rnd = np.random.RandomState(311)
+rnd = np.random.RandomState(1)
 
 biases = set_biases(bias_matrix_dim,copy.deepcopy(rnd))
 # Using the unsaturated version dramatically improves the first stages of learning
-weights = set_weights_unsaturated(weight_matrix_dim,copy.deepcopy(rnd))
+if(unsaturated_weights==True):
+    weights = set_weights_unsaturated(weight_matrix_dim,copy.deepcopy(rnd))
+else:
+    weights = set_weights(weight_matrix_dim,copy.deepcopy(rnd))
+
 
 train_evaluation = []
 timestamps = []
@@ -246,11 +275,6 @@ timestamps2 = []
 
 # Load the data, as zip iterables 
 training_data, test_data = loader.load_data()
-
-# Hyperparameter set 
-epochs = [5,5]
-mini_batches = [100,200]
-eta = [1.5,3]
 
 
 # Set error so to be reactive to overflow 
